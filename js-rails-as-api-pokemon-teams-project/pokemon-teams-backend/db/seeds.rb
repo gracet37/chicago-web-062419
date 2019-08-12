84fd1d39751dbd13c1b08bec24d48b35642556ec
NoahHeinrich <noawriteplay@gmail.com>
2019-08-12 16:22:20 -0500

    Adde better code

---
+++ js-rails-as-api-pokemon-teams-project/pokemon-teams-backend/db/seeds.rb
@@ -0,0 +1,38 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+Pokemon.delete_all
+
+Trainer.delete_all
+
+trainers_name = [
+  'Duke',
+  'Raza',
+  'Jess',
+  'Bruce',
+  'Derick',
+  'Noa',
+  'Adam',
+  'Shaan'
+]
+
+trainer_collection = []
+
+trainers_name.each do |name|
+  trainer_collection << Trainer.create(name: name)
+end
+
+trainer_collection.each do |trainer|
+  team_size = rand(6)
+
+  team_size.times do
+    name = Faker::Name.first_name
+    species = Faker::Games::Pokemon.name
+    Pokemon.create(nickname: name, species: species, trainer_id: trainer.id)
+  end
+end
